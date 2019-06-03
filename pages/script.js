f701332da98fff6a7099c297132d4849c062e7fc
ilvalamberte <38037680+ilvalamberte@users.noreply.github.com>
2019-06-03 15:03:49 +0200

    Add files via upload

---
+++ pages/script.js
@@ -0,0 +1,43 @@
+
+
+
+/*function function3()
+{
+    var elemsLocation = document.getElementsByClassName('layer--locations');
+    var elemsBet = document.getElementsByClassName('layer--betting');
+   if(elemsLocation[i].style.display == "block")
+  
+    for (var i=0;i<elems.length;i+=1){
+        elemsBet[i].style.display = 'block';
+    for (var i=0;i<elems.length;i+=1){
+        elemsLocation[i].style.display = 'none';
+      
+  
+}*/
+  
+function function1() {
+    document.getElementById("layer--location").style.display = "none";
+    document.querySelector(".st0").style.webkitAnimationPlayState = "running";
+
+  }
+
+
+      let names = document.querySelectorAll("#wrapperLoc div");
+  
+
+      function whatever(event) {
+          console.log(event.target.className);
+      }
+
+      function whatever2(event) {
+          console.log(event.target.className);
+      }
+
+
+      function myFunction() {
+        let inputNumber = console.log(document.getElementById("myNumber").value);
+      }
+
+      function calculateEnd() {
+          
+      }
